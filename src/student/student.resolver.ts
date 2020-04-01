acae44e8695bafb63699811b809dc73c2b87b990
Saulo Silva <saulo@enesolucoes.com.br>
2020-04-01 13:28:32 -0300

    Lista todos estudantes

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import {Args, Mutation, Query, Resolver} from '@nestjs/graphql';
 import { StudentType } from './student.type';
 import { StudentService } from './student.service';
 import { CreateStudentInput } from './create-student.input';
@@ -6,6 +6,11 @@ import { CreateStudentInput } from './create-student.input';
 @Resolver(of => StudentType)
 export class StudentResolver {
   constructor(private studentService: StudentService) {}
+
+  @Query(returns => [StudentType])
+  students() {
+    return this.studentService.getStudents();
+  }
 
   @Mutation(returns => StudentType)
   createStudent(
