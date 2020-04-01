8b05e32943db33e226808bd2103c1b49e790b33e
Saulo Silva <saulo@enesolucoes.com.br>
2020-04-01 11:25:03 -0300

    Ativa mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import {GraphQLModule} from "@nestjs/graphql";
 import { LessonModule } from './lesson/lesson.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Lesson} from "./lesson/lesson.entity";
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://0.0.0.0/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [
+          Lesson
+      ]
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true
     }),
