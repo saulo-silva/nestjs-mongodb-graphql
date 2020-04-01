9536b56625d18761c7655c557dac919f4ad296a2
Saulo Silva <saulo@enesolucoes.com.br>
2020-04-01 11:48:50 -0300

    Insert Mongo com graphql

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,8 +1,13 @@
-import {Query, Resolver} from "@nestjs/graphql";
+import {Args, Mutation, Query, Resolver} from "@nestjs/graphql";
 import {LessonType} from "./lesson.type";
+import {LessonService} from "./lesson.service";
 
 @Resolver(of => LessonType)
 export class LessonResolver {
+    constructor(
+        private lessonService: LessonService
+    ) {
+    }
     @Query(returns => LessonType)
     lesson() {
         return {
@@ -11,5 +16,14 @@ export class LessonResolver {
             startDate: (new Date()).toISOString(),
             endDate: (new Date()).toISOString()
         }
+    }
+
+    @Mutation(returns => LessonType)
+    createLession(
+        @Args('name') name: string,
+        @Args('startDate') startDate: string,
+        @Args('endDate') endDate: string,
+    ) {
+        return this.lessonService.createLesson(name, startDate, endDate)
     }
 }
