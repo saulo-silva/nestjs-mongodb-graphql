a75f7f8c2338b892a77f66ba99b811165c2af334
Saulo Silva <saulo@enesolucoes.com.br>
2020-04-01 13:23:22 -0300

    Cria estudante

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,1 +1,16 @@
-export class StudentResolver {}
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { StudentType } from './student.type';
+import { StudentService } from './student.service';
+import { CreateStudentInput } from './create-student.input';
+
+@Resolver(of => StudentType)
+export class StudentResolver {
+  constructor(private studentService: StudentService) {}
+
+  @Mutation(returns => StudentType)
+  createStudent(
+    @Args('createStudentInput') createStudentInput: CreateStudentInput,
+  ) {
+    return this.studentService.createStudent(createStudentInput);
+  }
+}
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Student } from './student.entity';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { CreateStudentInput } from './create-student.input';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
+
+  async createStudent(
+    createStudentInput: CreateStudentInput,
+  ): Promise<Student> {
+    const { firstName, lastName } = createStudentInput;
+    const student = this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+
+    return this.studentRepository.save(student);
+  }
+}
