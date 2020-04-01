89e9493be2d18c0b536581a1e7e241a632138311
Saulo Silva <saulo@enesolucoes.com.br>
2020-04-01 13:00:00 -0300

    Define estudantes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson/lesson.entity';
+import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
 
 @Module({
   imports: [
@@ -11,12 +13,13 @@ import { Lesson } from './lesson/lesson.entity';
       url: 'mongodb://0.0.0.0/school',
       synchronize: true,
       useUnifiedTopology: true,
-      entities: [Lesson],
+      entities: [Lesson, Student],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     LessonModule,
+    StudentModule,
   ],
   controllers: [],
   providers: [],
