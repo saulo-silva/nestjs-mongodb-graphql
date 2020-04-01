b4cecb8fb6943c4f9ec5db4fea4335a4916735ce
Saulo Silva <saulo@enesolucoes.com.br>
2020-04-01 10:55:03 -0300

    Insere packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import {GraphQLModule} from "@nestjs/graphql";
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true
+    })
+  ],
   controllers: [],
   providers: [],
 })
